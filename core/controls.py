08a7fd4289b43d5da7778ffe8d74f45ec2676d2c
TerenceGrover <terencegrover@gmail.com>
2025-07-16 22:39:55 -0700

    tryshit2

--- core/controls.py
+++ core/controls.py
@@ -8,7 +8,7 @@ class Controls:
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
-        self.left_pin = 14
+        self.left_pin = 15
         self.center_pin = 8
         self.right_pin = 25
 
