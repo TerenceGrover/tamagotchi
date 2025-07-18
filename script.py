847650f30e2547361538dbcb45cb519b3813475c
TerenceGrover <terencegrover@gmail.com>
2025-07-18 12:15:22 -0700

    return 0 ?

--- script.py
+++ script.py
@@ -320,7 +320,7 @@ def main():
             graphics.render_to_matrix()
             time.sleep(1.0 / FPS)
     
-        return states.stage_of_life
+        return 0
     
     if debug: 
         pygame.quit()
